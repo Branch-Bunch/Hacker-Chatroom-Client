4ad4ae08190502e45c6972a8af3dadd6a20175da
vikrambombhi <vbombhi@gmail.com>
2016-10-28 19:58:53 -0400

    added function to send user input to server, nothing has been tested yet

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 'use strict'
 
 const io = require("socket.io-client")
+const readline = require("readline")
+
 console.log('booting up HackerRoom-Client')
 
 const socket = io.connect('http://localhost:3030/')
 
+var rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+});
+
 socket.on('connect', function(data) {
-  console.log('connected')
+    console.log('connected')
+	sendMessage()
 })
 
 socket.on('invalid', (error) => {
@@ -16,3 +24,14 @@ socket.on('invalid', (error) => {
 socket.on('test', (data) => {
   console.log(data)
 })
+
+function sendMessage(){
+	rl.question("What do u wana send ", (answer) => {
+		socket.emit('general', {
+			name: 'Admin',
+			date: new Date(),
+			message: ${answer} 
+   		})
+	}
+ 	rl.close;
+}
