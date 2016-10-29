66cfa9b14835fcada0e478c2cbcb5ce3f87864e6
kirinrastogi <rastogikirin@gmail.com>
2016-10-28 21:52:24 -0400

    Add strings for testing on local/heroku, add prompt for username

--- app.js
+++ app.js
@@ -3,10 +3,12 @@
 const io = require("socket.io-client")
 const readline = require("readline")
 const rooms = require('./rooms.js')
+const local = 'http://localhost:3030'
+const heroku = 'https://hacker-chatroom.herokuapp.com'
 
 let username = "anonymous"
 // For local testing: http:localhost:3030
-const socket = io.connect('https://hacker-chatroom.herokuapp.com')
+const socket = io.connect(heroku)
 // const socket = io.connect('http://localhost:3030')
 
 let rl = readline.createInterface({
@@ -15,6 +17,7 @@ let rl = readline.createInterface({
 });
 
 socket.on('connect', (data) => {
+    setName()
     socket.emit('create', 'room1')
     console.log('Connected to Server')
     sendMessage()
